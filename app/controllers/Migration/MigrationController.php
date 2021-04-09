3e0b28dabef458a5a4fda5fa42b2538c1b2106ee
jagtheripper <jguapin21@gmail.com>
2021-04-09 08:17:30 +0800

    refactor
    when using $.post it will run through routes then all your ajax query will safe in the controller@method we can save another .php file

--- app/controllers/Migration/MigrationController.php
+++ app/controllers/Migration/MigrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Core\Request;
+use App\Core\Database\Migration\Migration;
 
 class MigrationController
 {
@@ -17,7 +18,39 @@ class MigrationController
 
 	public function run()
 	{
-		$request = Request::validate('login');
-		return view('migrations/run', compact('request'));
+		$request = Request::validate('migration');
+
+		$migration = new Migration();
+		$commandType = $request["command"];
+		$migrationName = $request["migrationName"];
+
+		switch ($commandType) {
+			case 'migration:make':
+				echo $migration->make($migrationName);
+				break;
+
+			case 'migration:migrate':
+				echo $migration->migrate();
+				break;
+
+			case 'migration:fresh':
+				echo $migration->fresh();
+				break;
+
+			case 'schema:dump':
+				echo $migration->dump();
+				break;
+
+			case 'schema:dump-prune':
+				echo $migration->dump('prune');
+				break;
+
+			case 'migration:rollback':
+				echo $migration->rollback();
+				break;
+
+			default:
+				break;
+		}
 	}
 }
