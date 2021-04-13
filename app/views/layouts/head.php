9bddfbbf81276248bb12c9e5adb1229b9a8f66cf
jagwarthegreat <jguapin21@gmail.com>
2021-04-13 20:02:45 +0800

    embed jquery and bootstrap

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -14,7 +14,7 @@ use App\Core\Auth;
 		<?= ucfirst($pageTitle) . " | " . App::get('config')['app']['name'] ?>
 	</title>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= public_url('/assets/css/bootstrap.min.css') ?>">
 
 	<style>
 		body {
@@ -23,9 +23,9 @@ use App\Core\Auth;
 		}
 	</style>
 
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="<?= public_url('/assets/js/jquery-3.6.0.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/js/popper.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/js/bootstrap.min.js') ?>"></script>
 </head>
 
 <body>
--- app/views/migrations/index.view.php
+++ app/views/migrations/index.view.php
@@ -7,13 +7,13 @@ use App\Core\App;
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" cosntent="width=device-width, initial-scale=1.0">
 	<link rel='icon' href='<?= public_url('/favicon.ico') ?>' type='image/ico' />
 	<title>
 		<?= ucfirst($pageTitle) . " | " . App::get('config')['app']['name']; ?>
 	</title>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= public_url('/assets/css/bootstrap.min.css') ?>">
 
 	<style>
 		body {
@@ -21,9 +21,9 @@ use App\Core\App;
 		}
 	</style>
 
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="<?= public_url('/assets/js/jquery-3.6.0.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/js/popper.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/js/bootstrap.min.js') ?>"></script>
 </head>
 
 <body>
