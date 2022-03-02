e3deb7db3a7dc20e05d52772c47df4b0cc4b8f89
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-03-02 15:14:09 +0800

    Add files via upload

---
+++ database/migrations/20210408051901_create_permission_table.php
@@ -0,0 +1,15 @@
+<?php
+
+$create_permission_table = [
+	"mode" => "NEW",
+	"table"	=> "permissions",
+	"primary_key" => "id",
+	"up" => [
+		"id" => "int(11) NOT NULL AUTO_INCREMENT",
+		"title" => "text NOT NULL",
+		"date" => "date NOT NULL",
+	],
+	"down" => [
+		"" => ""
+	]
+];
